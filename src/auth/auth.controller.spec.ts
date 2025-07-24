322de8d4d9ecbedf0d97ad9f9336fc9dbcaec8d3
Emerson Fan <metalraccoon@gmail.com>
2025-07-25 04:35:17 +0800

    test: add sign tests and adjust format

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ConflictException } from '@nestjs/common';
+import { ConflictException, UnauthorizedException } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 describe('AuthController', () => {
@@ -10,6 +10,7 @@ describe('AuthController', () => {
 
   const mockAuthService = {
     signup: jest.fn(),
+    signin: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -25,50 +26,89 @@ describe('AuthController', () => {
     jest.clearAllMocks();
   });
 
-  it('should call authService.signup and redirect with success lash', async () => {
-    const mockReq = {
-      session: {},
-    } as unknown as Request;
+  describe('signup', () => {
+    it('should call authService.signup and redirect with success flash', async () => {
+      const mockReq = {
+        session: {} as Record<string, any>,
+      } as unknown as Request;
 
-    const mockRes = {
-      redirect: jest.fn(),
-    } as unknown as Response;
-    const dto = {
-      name: 'test',
-      email: 'test@test.com',
-      password: 'test',
-    };
+      const mockRes = {
+        redirect: jest.fn(),
+      } as unknown as Response;
+      const dto = {
+        name: 'test',
+        email: 'test@test.com',
+        password: 'test',
+      };
 
-    mockAuthService.signup.mockResolvedValueOnce(undefined);
-    await authController.signup(mockReq, dto, mockRes);
-    expect(authService.signup).toHaveBeenCalledWith(dto);
-    expect(mockReq.session.flash).toEqual({
-      type: 'success',
-      message: 'Account apply succeed, please login!',
+      mockAuthService.signup.mockResolvedValueOnce(undefined);
+      await authController.signup(mockReq, dto, mockRes);
+      expect(authService.signup).toHaveBeenCalledWith(dto);
+      expect(mockReq.session.flash).toEqual({
+        type: 'success',
+        message: 'Account apply succeed, please login!',
+      });
+      expect(mockRes.redirect).toHaveBeenCalledWith('/');
     });
-    expect(mockRes.redirect).toHaveBeenCalledWith('/');
+    it('should set error flash and redirect if email already taken', async () => {
+      const mockReq = {
+        session: {},
+      } as unknown as Request;
+
+      const mockRes = {
+        redirect: jest.fn(),
+      } as unknown as Response;
+      const dto = {
+        name: 'test',
+        email: 'test@test.com',
+        password: 'test',
+      };
+
+      mockAuthService.signup.mockRejectedValueOnce(new ConflictException());
+      await authController.signup(mockReq, dto, mockRes);
+      expect(authService.signup).toHaveBeenCalledWith(dto);
+      expect(mockReq.session.flash).toEqual({
+        type: 'error',
+        message: 'Email already taken',
+      });
+      expect(mockRes.redirect).toHaveBeenCalledWith('/auth/signup');
+    });
   });
-  it('should set error flash and redirect if email already taken', async () => {
-    const mockReq = {
-      session: {},
-    } as unknown as Request;
-
-    const mockRes = {
-      redirect: jest.fn(),
-    } as unknown as Response;
-    const dto = {
-      name: 'test',
-      email: 'test@test.com',
-      password: 'test',
-    };
-
-    mockAuthService.signup.mockRejectedValueOnce(new ConflictException());
-    await authController.signup(mockReq, dto, mockRes);
-    expect(authService.signup).toHaveBeenCalledWith(dto);
-    expect(mockReq.session.flash).toEqual({
-      type: 'error',
-      message: 'Email already taken',
+  describe('signin', () => {
+    it('should call authService.signin, add jwt at cookie and redirect to users/home', async () => {
+      const mockReq = { session: {} } as unknown as Request;
+      const mockRes = {
+        cookie: jest.fn(),
+        redirect: jest.fn(),
+      } as unknown as Response;
+      const dto = {
+        email: 'test@test.com',
+        password: 'test',
+      };
+      mockAuthService.signin.mockResolvedValueOnce({
+        access_token: 'jwtToken',
+      });
+      await authController.signin(mockReq, dto, mockRes);
+      expect(mockRes.cookie).toHaveBeenCalledWith('jwt', 'jwtToken');
+      expect(mockRes.redirect).toHaveBeenLastCalledWith('/users/home');
     });
-    expect(mockRes.redirect).toHaveBeenCalledWith('/auth/signup');
+    it('should catch 401 unauthorized, set session.flash and redirect to /auth/signin', async () => {
+      const mockReq = { session: {} } as unknown as Request;
+      const mockRes = {
+        cookie: jest.fn(),
+        redirect: jest.fn(),
+      } as unknown as Response;
+      const dto = {
+        email: 'test@test.com',
+        password: 'test',
+      };
+      mockAuthService.signin.mockRejectedValueOnce(new UnauthorizedException());
+      await authController.signin(mockReq, dto, mockRes);
+      expect(mockReq.session.flash).toEqual({
+        type: 'error',
+        message: 'Invalid credentials',
+      });
+      expect(mockRes.redirect).toHaveBeenCalledWith('/auth/signin');
+    });
   });
 });
