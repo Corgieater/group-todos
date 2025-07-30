3fabef7f357e0148e870b3d6f37e992c95938b4d
Emerson Fan <metalraccoon@gmail.com>
2025-07-30 11:03:39 +0800

    test: auth.controller.changePassword (success, 403, 400)

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ConflictException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  ForbiddenException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 import {
   createMockSignupDto,
   createMockSigninDto,
 } from 'src/test/factories/mock-user.factory';
-
+import { UserPayload } from 'src/common/types/user-payload';
+import { AuthUpdatePasswordDto } from './dto/auth.dto';
+import { AuthUpdatePasswordPayload } from './types/auth';
 describe('AuthController', () => {
   let authController: AuthController;
 
   let mockReq: Request;
   let mockRes: Response;
+  let mockUser: UserPayload;
 
   let authSignupDto: {
     name: string;
@@ -28,6 +36,7 @@ describe('AuthController', () => {
   const mockAuthService = {
     signup: jest.fn(),
     signin: jest.fn(),
+    changePassword: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -40,6 +49,12 @@ describe('AuthController', () => {
       cookie: jest.fn(),
       clearCookie: jest.fn(),
     } as unknown as Response;
+
+    mockUser = {
+      userId: 1,
+      userName: 'test',
+      email: 'test@test.com',
+    };
 
     authSignupDto = createMockSignupDto();
     authSigninDto = createMockSigninDto();
@@ -106,6 +121,54 @@ describe('AuthController', () => {
         message: 'Signed out successfully',
       });
       expect(mockRes.redirect).toHaveBeenCalledWith('/');
+    });
+  });
+  describe('changePassword', () => {
+    let dto: AuthUpdatePasswordDto;
+    let payload: AuthUpdatePasswordPayload;
+    beforeEach(() => {
+      dto = {
+        oldPassword: 'test',
+        newPassword: 'foo',
+      };
+      payload = {
+        ...mockUser,
+        ...dto,
+      };
+    });
+    it('should change user password, clear cookie and redirect to /', async () => {
+      await authController.changePassword(mockReq, mockUser, dto, mockRes);
+      expect(mockAuthService.changePassword).toHaveBeenCalledWith(payload);
+      expect(mockReq.session.flash).toEqual({
+        type: 'success',
+        message: 'Password changed',
+      });
+      expect(mockRes.clearCookie).toHaveBeenCalledWith('jwt');
+      expect(mockRes.redirect).toHaveBeenCalledWith('/');
+    });
+
+    it('should set error flash message Old password is incorrect and redirect to /users/home', async () => {
+      mockAuthService.changePassword.mockRejectedValueOnce(
+        new ForbiddenException('Old password is incorrect'),
+      );
+      await authController.changePassword(mockReq, mockUser, dto, mockRes);
+      expect(mockReq.session.flash).toEqual({
+        type: 'error',
+        message: 'Old password is incorrect',
+      });
+      expect(mockRes.redirect).toHaveBeenCalledWith('/users/home');
+    });
+
+    it('should set error flash message Please use a new password and redirect to /users/home', async () => {
+      mockAuthService.changePassword.mockRejectedValueOnce(
+        new BadRequestException('Please use a new password'),
+      );
+      await authController.changePassword(mockReq, mockUser, dto, mockRes);
+      expect(mockReq.session.flash).toEqual({
+        type: 'error',
+        message: 'Please use a new password',
+      });
+      expect(mockRes.redirect).toHaveBeenCalledWith('/users/home');
     });
   });
 });
