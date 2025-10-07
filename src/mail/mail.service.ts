455cbbef9e240b85401679a05a1575ecc610ad42
Emerson Fan <metalraccoon@gmail.com>
2025-10-07 21:47:28 +0800

    refactor(mail)!: add more context

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -5,12 +5,28 @@ import { User } from '@prisma/client';
 @Injectable()
 export class MailService {
   constructor(private readonly mailService: MailerService) {}
-  async sendMail(user: User, link: string) {
+
+  async sendPasswordReset(user: User, link: string) {
     await this.mailService.sendMail({
       to: user.email,
       subject: 'Password Reset',
       template: 'auth/reset-password-letter',
       context: { name: user.name, link },
+    });
+  }
+
+  async sendGroupInvite(
+    email: string,
+    inviteeName: string | undefined,
+    link: string,
+    inviterName: string,
+    groupName: string,
+  ) {
+    await this.mailService.sendMail({
+      to: email,
+      subject: 'You are invited to join a group',
+      template: 'groups/invite-letter',
+      context: { name: inviteeName, email, link, inviterName, groupName },
     });
   }
 }
